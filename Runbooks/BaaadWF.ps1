35bba138fd5861ced806103ca1caf3303b2ea944
Beth Cooper <ecoop@seas.upenn.edu>
2015-08-27 15:23:03 -0700

    Adding a runbook that should fail to sync to source control

---
+++ Runbooks/BaaadWF.ps1
@@ -0,0 +1,7 @@
+﻿<#This is a malformed runbook.  Should fail to sync to Automation Source Control#>
+workfow BaaadWF {
+
+    Write-Output "Lalalala"
+
+} 
+Write-Output "I am not where I should be!"
