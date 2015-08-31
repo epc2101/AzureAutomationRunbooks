4a97c871cb6e6ada22f4bccc1b13d9d8c36dd999
Beth Cooper <ecoop@seas.upenn.edu>
2015-08-31 16:50:25 -0700

    Test empty content - should fail to sync

--- Runbooks/BaaadWF.ps1
+++ Runbooks/BaaadWF.ps1
@@ -1,8 +1,2 @@
-<# 
-	This PowerShell script was automatically converted to PowerShell Workflow so it can be run as a runbook.
-	Specific changes that have been made are marked with a comment starting with “Converter:”
-#>
-<#This is a malformed runbook.  Should fail to sync to Automation Source Control#>
-workflow BaaadWF {
-	
+
 	
